1a552425736203b448983012c9bc58959bb20b5a
Akshit Bansal <bansalakshit56@gmail.com>
2018-01-21 00:09:59 +0530

    Update MyFirebaseMessagingService.java

--- app/src/main/java/njr/com/techjalsa/service/MyFirebaseMessagingService.java
+++ app/src/main/java/njr/com/techjalsa/service/MyFirebaseMessagingService.java
@@ -1,7 +1,11 @@
-package njr.com.techjalsa.service;
+package njr.com.techjalsav2.service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.text.TextUtils;
 
@@ -11,9 +15,10 @@ import com.google.firebase.messaging.RemoteMessage;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import njr.com.techjalsa.app.Config;
-import njr.com.techjalsa.homeTabs.MainActivity;
-import njr.com.techjalsa.util.NotificationUtils;
+import njr.com.techjalsav2.R;
+import njr.com.techjalsav2.app.Config;
+import njr.com.techjalsav2.homeTabs.MainActivity;
+import njr.com.techjalsav2.util.NotificationUtils;
 
 /**
  * Created by dell on 21-02-2017.
@@ -21,68 +26,83 @@ import njr.com.techjalsa.util.NotificationUtils;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-    private NotificationUtils notificationUtils;
-
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        if (remoteMessage == null)
-            return;
-        if (remoteMessage.getNotification() != null) {
-            handleNotification(remoteMessage.getNotification().getBody());
-        }
-        if (remoteMessage.getData().size() > 0) {
-            try {
-                JSONObject json = new JSONObject(remoteMessage.getData().toString());
-                handleDataMessage(json);
-            } catch (Exception e) {
-            }
-        }
-    }
-    private void handleNotification(String message) {
-        if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
-            Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
-            pushNotification.putExtra("message", message);
-            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
-        }else{
-        }
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setContentTitle(remoteMessage.getNotification().getTitle());
+        notificationBuilder.setContentText(remoteMessage.getNotification().getBody());
+        notificationBuilder.setAutoCancel(true);
+        notificationBuilder.setSmallIcon(R.drawable.logo);
+        notificationBuilder.setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,notificationBuilder.build());
     }
 
-    private void handleDataMessage(JSONObject json) {
-        try {
-            JSONObject data = json.getJSONObject("data");
-
-            String title = data.getString("title");
-            String message = data.getString("message");
-            boolean isBackground = data.getBoolean("is_background");
-            String imageUrl = data.getString("image");
-            String timestamp = data.getString("timestamp");
-            JSONObject payload = data.getJSONObject("payload");
-            if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
-                Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
-                pushNotification.putExtra("message", message);
-                LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
-            } else {
-                Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
-                resultIntent.putExtra("message", message);
-                if (TextUtils.isEmpty(imageUrl)) {
-                    showNotificationMessage(getApplicationContext(), title, message, timestamp, resultIntent);
-                } else {
-                    showNotificationMessageWithBigImage(getApplicationContext(), title, message, timestamp, resultIntent, imageUrl);
-                }
-            }
-        } catch (JSONException e) {
-        } catch (Exception e) {
-        }
-    }
-    private void showNotificationMessage(Context context, String title, String message, String timeStamp, Intent intent) {
-        notificationUtils = new NotificationUtils(context);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        notificationUtils.showNotificationMessage(title, message, timeStamp, intent);
-    }
-
-    private void showNotificationMessageWithBigImage(Context context, String title, String message, String timeStamp, Intent intent, String imageUrl) {
-        notificationUtils = new NotificationUtils(context);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        notificationUtils.showNotificationMessage(title, message, timeStamp, intent, imageUrl);
-    }
+//    private NotificationUtils notificationUtils;
+//
+//    @Override
+//    public void onMessageReceived(RemoteMessage remoteMessage) {
+//        if (remoteMessage == null)
+//            return;
+//        if (remoteMessage.getNotification() != null) {
+//            handleNotification(remoteMessage.getNotification().getBody());
+//        }
+//        if (remoteMessage.getData().size() > 0) {
+//            try {
+//                JSONObject json = new JSONObject(remoteMessage.getData().toString());
+//                handleDataMessage(json);
+//            } catch (Exception e) {
+//            }
+//        }
+//    }
+//    private void handleNotification(String message) {
+//        if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
+//            Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
+//            pushNotification.putExtra("message", message);
+//            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+//        }else{
+//        }
+//    }
+//
+//    private void handleDataMessage(JSONObject json) {
+//        try {
+//            JSONObject data = json.getJSONObject("data");
+//
+//            String title = data.getString("title");
+//            String message = data.getString("message");
+//            boolean isBackground = data.getBoolean("is_background");
+//            String imageUrl = data.getString("image");
+//            String timestamp = data.getString("timestamp");
+//            JSONObject payload = data.getJSONObject("payload");
+//            if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
+//                Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
+//                pushNotification.putExtra("message", message);
+//                LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+//            } else {
+//                Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
+//                resultIntent.putExtra("message", message);
+//                if (TextUtils.isEmpty(imageUrl)) {
+//                    showNotificationMessage(getApplicationContext(), title, message, timestamp, resultIntent);
+//                } else {
+//                    showNotificationMessageWithBigImage(getApplicationContext(), title, message, timestamp, resultIntent, imageUrl);
+//                }
+//            }
+//        } catch (JSONException e) {
+//        } catch (Exception e) {
+//        }
+//    }
+//    private void showNotificationMessage(Context context, String title, String message, String timeStamp, Intent intent) {
+//        notificationUtils = new NotificationUtils(context);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        notificationUtils.showNotificationMessage(title, message, timeStamp, intent);
+//    }
+//
+//    private void showNotificationMessageWithBigImage(Context context, String title, String message, String timeStamp, Intent intent, String imageUrl) {
+//        notificationUtils = new NotificationUtils(context);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        notificationUtils.showNotificationMessage(title, message, timeStamp, intent, imageUrl);
+//    }
 }
